fc2bb49d5bd8068d10895d806251ef88baa1e24f
CrazyCoder76 <gang.expert.99@gmail.com>
2024-01-16 13:58:08 -0500

    update request method

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ var PORT = 8080;
 var HOST_NAME = '127.0.0.1';
 var DATABASE_NAME = 'bookUrlList';
 
-var request_count = 100;
-var request_index = 0;
-var interval = null;
+var maxPages = 5;
 
 mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
 
@@ -29,39 +27,36 @@ app.listen(PORT, function () {
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 async function sendRequest(pageId, category) {
+  console.log(pageId);
   axios.get(`https://www.everand.com/books/${category}/explore-more?page=${pageId}`)
       .then(response => {
         const data = response.data;
+        var cnt = 0;
         for(let key in data.documents) {
           let book_instance = new bookSchema({title: data.documents[key].title, url: data.documents[key].previewUrl});
 
           book_instance.save()
             .then(res => {
-              console.log(res);
+              console.log(res.title);
             })
             .catch(error => {
               console.log(error);
             });
+          cnt++;
         }
+        console.log(cnt);
+        if(pageId < maxPages)
+          sendRequest(pageId+1, category);
       })
       .catch(error => {
         console.log(error);
       })
 }
-async function sendRequests() {
-  let startId = request_index * request_count + 1;
-  let endId = (request_index+1) * request_count;
-  request_index ++;
-  for(let i = startId; i <= endId; i++)
-    await sendRequest(i, "Computers");
-  if(request_index > 10000) 
-    clearInterval(interval);
-}
 
 mongoose.connection.once('open', function () {
   console.log('Connected to MongoDB');
 
-  interval = setInterval(sendRequests, 2000);
+  sendRequest(1, "Computers");
 });
 
 
